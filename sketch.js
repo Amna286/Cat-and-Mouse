32ef72d4a25e6c8048852ca02bf089cefe63c488
Amna286 <75910591+Amna286@users.noreply.github.com>
2021-01-29 17:51:28 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,41 @@
+var cat, mouse;
+var catImage, mouseImage, catAnimation, mouseAnimation;
+var backgroundImage;
+
+function preload() {
+    catImage = loadImage("tomOne.png");
+    mouseImage = loadImage("jerryOne.png");
+    catAnimation = loadAnimation("tomOne.png", "tomTwo.png", "tomThree.png", "tomFour.png");
+    mouseAnimation = loadAnimation("jerryOne.png", "jerryTwo.png", "jerryThree.png", "jerryFour.png");
+    backgroundImage = loadImage("garden.png");
+}
+
+function setup(){
+    createCanvas(1000,800);
+    cat = createSprite(700, 600);
+    cat.addImage(catImage);
+
+    mouse = createSprite(300, 600);
+    mouse.addImage(mouseImage);
+}
+
+function draw() {
+    background(backgroundImage);
+    keyPressed();
+
+    if(cat.x - mouse.x < (cat.width - mouse.width) / 2) {
+        cat.addAnimation("catLastImage", catImage);
+        cat.velocityX = 0;
+    }
+
+    drawSprites();
+}
+
+
+function keyPressed(){
+    if(keyDown === LEFT_ARROW) {
+        cat.velocityX = -5;
+        cat.addAnimation("catRunning", catAnimation);
+        cat.changeAmimation("catRunning");
+    }
+}
